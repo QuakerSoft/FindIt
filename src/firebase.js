74a3f4c37e6d05d813f505480bf81721cc8a32c4
Pressley <imprezz.pressley.402@my.csun.edu>
2026-07-20 23:59:29 -0700

    Set up Firebase Authentication and Firestore

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDOCpLFrBpgej9sBR8KCmyiio1c75Kd2IU",
+  authDomain: "findit-csun.firebaseapp.com",
+  projectId: "findit-csun",
+  storageBucket: "findit-csun.firebasestorage.app",
+  messagingSenderId: "342993552133",
+  appId: "1:342993552133:web:a378e860c650163364ad6c",
+  measurementId: "G-MP4NLJ4BKE"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
