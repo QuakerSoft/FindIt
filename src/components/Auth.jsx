c52ab853ec918bbfef5e13f22fee1abe008e7e39
Pressley <imprezz.pressley.402@my.csun.edu>
2026-07-21 02:06:00 -0700

    Add signup login and Firestore user profiles

---
+++ src/components/Auth.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { db } from "../firebase";
+import { doc, setDoc } from "firebase/firestore";
+
+function Auth() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const signUp = async () => {
+        try {
+            const userCredential =
+                await createUserWithEmailAndPassword(auth, email, password);
+                await setDoc(
+                    doc(db, "users", userCredential.user.uid),
+                    {
+                        email: email,
+                        role: "student",
+                    }
+                );
+            alert("Account created successfully!");
+        } catch (error) {
+            alert(error.message);
+        }
+
+    };
+
+    const logIn = async () => {
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            alert("Logged in!");
+        } catch (error) {
+            alert(error.message);
+        }
+    };
+
+    return (
+        <div>
+            <h2>FindIt Login</h2>
+
+            <input
+            type="email"
+            placeholder="Email"
+            calue={email}
+            onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br /><br />
+
+        <button onClick={signUp}>Sign Up</button>{" "}
+
+        <button onClick={logIn}>Log In</button>
+        </div>
+    );
+}
+
+export default Auth;
