bc96f2d9186f1f297306fdb43d35844cab980dbf
martinezisamar <martinezisamar819@gmail.com>
2026-07-21 15:17:35 -0700

    adding header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import Auth from "./components/Auth";
 
+function Header() {
+  return (
+    <div>
+      <h1>FindIt CSUN</h1>
+      <h2>Cal State Northridge Lost and Found Site</h2>
+    </div>
+  )
+}
+
 function App() {
-  return <Auth />;
+  return (
+    <>
+      <Auth />
+      <Header />
+    </>
+  );
 }
 
 export default App;
