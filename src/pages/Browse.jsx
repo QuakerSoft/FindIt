08dc9017411ef9f69306f2ce7ab8a5243582ccdf
Devin Como <dcomo16@gmail.com>
2026-07-22 12:49:59 -0700

    Added Auth, Layout and Navbar Components

---
+++ src/pages/Browse.jsx
@@ -0,0 +1,5 @@
+function Browse() {
+  return <h1 className="text-2xl font-semibold">Browse Items</h1>;
+}
+
+export default Browse;
---
+++ src/pages/PostItem.jsx
@@ -0,0 +1,4 @@
+function PostItem() {
+  return <h1 className="text-2xl font-semibold">Post an Item</h1>;
+}
+export default PostItem;
