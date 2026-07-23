ee04daf2a1a6fb038efa0f54b3ab8a05fc4a4bc8
martinezisamar <martinezisamar819@gmail.com>
2026-07-23 10:42:34 -0700

    added margin space to browse page

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -10,7 +10,7 @@ function Browse() {
         Search Lost and Found Items
       </h1>
       
-      <p className="my-2 text-sm text-slate-500">
+      <p className="mt-2 mb-4 text-sm text-slate-500">
         View items reported by members of the CSUN community.
       </p>
 
